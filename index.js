aa1674f5b0dea4f1e89293f43f713117ea7f8b03
Matteo Valles <matteovalles@gmail.com>
2018-09-09 23:49:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine.shift
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
