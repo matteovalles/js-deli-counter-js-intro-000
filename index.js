e6d63e7805ed998a5d8d0663a3165d03a42ca322
Matteo Valles <matteovalles@gmail.com>
2018-09-09 23:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function takeANumber(line, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
+    var temp = katzDeliLine[0]
     katzDeliLine.shift
-    return `Currently serving ${katzDeliLine[0].}`
+    return `Currently serving ${temp}.`
   } else {
     return 'There is nobody waiting to be served!'
   }
