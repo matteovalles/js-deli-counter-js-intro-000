869164b4ceedfaddf1a4b6960fe28b0968149e5e
Matteo Valles <matteovalles@gmail.com>
2018-09-10 00:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    var string = 'The line is currently:'
+    for (let i = 0; i < line.length; i++) {
+      string = `${string} ${i+1}. ${line[i]}`
+    }
+  }
+  return string
+}
