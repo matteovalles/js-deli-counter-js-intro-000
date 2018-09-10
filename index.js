47db11acc12fadc2bdc7834e4c27caa43363104e
Matteo Valles <matteovalles@gmail.com>
2018-09-10 00:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function takeANumber(line, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return 'The line is currently empty.'
+    return 'There is nobody waiting to be served!'
   } else {
-    return `Currently serving ${katzDeliLine.shift()}`
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
